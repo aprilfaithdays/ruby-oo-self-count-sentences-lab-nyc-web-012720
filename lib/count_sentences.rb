e4c251ef85bada18ba813b367f93fdf2183819bc
aprilfaithdays <aprilfaith.e@gmail.com>
2020-02-01 20:01:32 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.sentence? || self.question? || self.exclamation?
+    count_this = self.split(/\. |\? |\! /)
+    count_this.count
   end
 end
